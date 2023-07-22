1081320039df0742b0896575de027d65c882ca35
Daly <dalyna64@gmail.com>
2023-07-22 14:05:35 +0100

    adding multer and file uploading system

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,21 @@ const router = express.Router();
 
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
+const multer = require("multer");
+
+let fileName = "";
+
+const myStorage = multer.diskStorage({
+  destination: "./uploades/users",
+  filename: (req, file, redirect) => {
+    let date = Date.now();
+    let fl = date + "." + file.mimetype.split("/")[1];
+    redirect(null, fl);
+    fileName = fl;
+  },
+});
+
+const upload = multer({ storage: myStorage });
 
 router.get("/", async (req, res) => {
   try {
@@ -22,9 +37,10 @@ router.get("/get/:id", async (req, res) => {
   }
 });
 
-/* router.post("/add", async (req, res) => {
+/* router.post("/add", upload.any("image"), async (req, res) => {
   try {
     user = new User(req.body);
+    user.image = fileName;
     savedUser = await user.save();
     res.status(200).send(savedUser);
   } catch (err) {
@@ -32,12 +48,13 @@ router.get("/get/:id", async (req, res) => {
   }
 }); */
 
-router.post("/register", async (req, res) => {
+router.post("/register", upload.any("image"), async (req, res) => {
   try {
     user = new User(req.body);
     salt = bcrypt.genSaltSync(10);
     cryptedPass = await bcrypt.hashSync(user.password, salt);
     user.password = cryptedPass;
+    user.image = fileName;
     savedUser = await user.save();
     res.status(200).json(savedUser);
   } catch (err) {
@@ -50,17 +67,17 @@ router.post("/login", async (req, res) => {
     user = await User.findOne({ name: data.name });
 
     if (!user) {
-      res.status(404).send("name invalid");
+      res.status(404).json({ message: "data invalid" });
     } else {
       validpassword = await bcrypt.compareSync(data.password, user.password);
       if (!validpassword) {
-        res.status(404).json(" password invalid");
+        res.status(404).send({ message: "data invalid" });
       } else {
-        res.status(200).json(user.name);
+        res.status(200).send({ message: "login Ok" });
       }
     }
   } catch (error) {
-    res.status(400).json(err);
+    res.status(400).json(error);
   }
 });
 
